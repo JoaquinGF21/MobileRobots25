e40608630d313fb8b3ed374c744782f7dcb77fb9
Rasp <joaquinkiwi04@gmail.com>
2025-02-07 01:35:29 -0500

    v1.0.2 push from rasp

--- jacob_v2.py
+++ jacob_v2.py
@@ -5,7 +5,7 @@ import signal
 import threading
 import math
 
-adjl = .0463
+adjl = .0465
 pigpi = pigpio.pi()
 
 controller = robot_controller.control(pi=pigpi)
@@ -34,10 +34,11 @@ def calculate_distance(acceleration,dt):
     velocity += acceleration *dt
     
     distance += velocity *dt
-    return distance
+    return distance * 100
 
 def move_straight(control,speed,distance,tick_speed):
     current = time.time()
+    last = time.time()
     controller.sampling_time = tick_speed
     controller.set_speed_l(speed + adjl)
     controller.set_speed_r(speed)
@@ -48,20 +49,19 @@ def move_straight(control,speed,distance,tick_speed):
         # print("Heading: " + str(180 + math.atan2(values[1], values[0]) * 180 / math.pi))
         accel = controller.imu.linear_acceleration
         #try needed because last isn't initialized yet
-        try:
-            if distance >= calculate_distance(accel[1],current - last):
-                pos = True
-                break
-            else:
-                pass
-        except Exception:
+        print(calculate_distance(accel[1],current - last))
+        if distance <= calculate_distance(accel[1],current - last):
+            pos = True
+            break
+        else:
             pass
+        
         time.sleep(controller.sampling_time -
                    ((time.time() - current) % controller.sampling_time))
         last = current
         #use accel[1]
-        
+    stop()
 
     
-    
-move_straight(controller,0.5,15,.02)
+# relativily 1m seems to vary about 10cm
+move_straight(controller,0.25,340,.03)
