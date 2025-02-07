ad8f8cc0c32c4f58e93e9e65497f85cfdcc75eb1
Jacob <jacobkyleoct@outlook.com>
2025-02-07 00:39:47 -0500

    v1.0.2 working straight function?

--- jacob_v2.py
+++ jacob_v2.py
@@ -20,24 +20,47 @@ signal.signal(signal.SIGINT,SignalHandler_SIGINT)
 
 
 def stop():
-      controller.set_speed_r(0)
-      controller.set_speed_l(0)
+    controller.set_speed_r(0)
+    controller.set_speed_l(0)
+    return None
+#{x,y,z} for linear_acceleration
+#dt is the time interval between use of the function
+# I took the skeleton of this function from Clause ai
+velocity = 0
+distance = 0
+def calculate_distance(acceleration,dt):
+    
+    velocity += acceleration *dt
+    
+    distance += velocity *dt
+    return distance
 
 def move_straight(control,speed,distance,tick_speed):
-    start = time.time()
+    current = time.time()
     controller.sampling_time = tick_speed
     controller.set_speed_l(speed + adjl)
     controller.set_speed_r(speed)
-    while time.time() != start + 2:
-        loop_time = time.time()
-        values = controller.imu.magnetic
-        print("Heading: " + str(180 + math.atan2(values[1], values[0]) * 180 / math.pi))
-#{x,y,z} for linear_acceleration
+    pos = False
+    while not pos:
+        current = time.time()
+        # values = controller.imu.magnetic
+        # print("Heading: " + str(180 + math.atan2(values[1], values[0]) * 180 / math.pi))
+        accel = controller.imu.linear_acceleration
+        #try needed because last isn't initialized yet
+        try:
+            if distance >= calculate_distance(accel[1],last - current):
+                pos = True
+                break
+            else:
+                pass
+        except Exception:
+            pass
         time.sleep(controller.sampling_time -
-                   ((time.time() - loop_time) % controller.sampling_time))
-        print('{:.20f}'.format((time.time() - loop_time)))
+                   ((time.time() - current) % controller.sampling_time))
+        last = current
+        #use accel[1]
+        
 
-    stop()
     
     
 move_straight(controller,0.5,15,.02)
