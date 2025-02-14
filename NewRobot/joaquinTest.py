ab48f2e40f303a866631e3f10ce0b8d0bd7294b6
Joaquin <joaquing@usf.edu>
2025-02-14 07:56:38 -0500

    v1.21B - Added robot.check_speed

--- NewRobot/joaquinTest.py
+++ NewRobot/joaquinTest.py
@@ -2,7 +2,9 @@ from robot_systems.robot import HamBot
 
 robot = HamBot
 
-robot.run_left_motor_for_seconds(robot, 3, speed=25, blocking=False)
-robot.run_right_motor_for_seconds(robot, 3, speed=25, blocking=False)
+robot.check_speed(robot, input_speed=25)
+
+robot.run_left_motor_for_seconds(robot, 3.0, speed=25, blocking=False)
+robot.run_right_motor_for_seconds(robot, 3.0, speed=25, blocking=False)
 
 robot.stop_motors()
