19276dc2116e3240ae5b4769cff428b923647cb9
Joaquin <joaquing@usf.edu>
2025-02-14 08:01:47 -0500

    v1.22 - Fixed instance of robot. - I previously had it as "robot = HamBot", when it should have been "robot = HamBot()" so it could creat an instance, not a simple assignment.

--- NewRobot/joaquinTest.py
+++ NewRobot/joaquinTest.py
@@ -1,10 +1,10 @@
 from robot_systems.robot import HamBot
 
-robot = HamBot
+robot = HamBot()
 
 robot.check_speed(robot, input_speed=25)
 
-robot.run_left_motor_for_seconds(robot, 3.0, speed=25, blocking=False)
-robot.run_right_motor_for_seconds(robot, 3.0, speed=25, blocking=False)
+robot.run_left_motor_for_seconds(3.0, speed=25, blocking=False)
+robot.run_right_motor_for_seconds(3.0, speed=25, blocking=False)
 
 robot.stop_motors()
