b99ad0522ccdeb8ec989a94b644c2f02bc1d666f
Joaquin <joaquing@usf.edu>
2025-02-14 08:02:48 -0500

    v1.22 - Fix

--- NewRobot/joaquinTest.py
+++ NewRobot/joaquinTest.py
@@ -2,8 +2,6 @@ from robot_systems.robot import HamBot
 
 robot = HamBot()
 
-robot.check_speed(robot, input_speed=25)
-
 robot.run_left_motor_for_seconds(3.0, speed=25, blocking=False)
 robot.run_right_motor_for_seconds(3.0, speed=25, blocking=False)
 
