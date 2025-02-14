fb1178cf6efacae59acdadd913f95421ce795a98
Joaquin <joaquing@usf.edu>
2025-02-14 08:49:33 -0500

    v1.24 - ACTUALLY, made proper use of the moveForward function

--- NewRobot/joaquinTest.py
+++ NewRobot/joaquinTest.py
@@ -5,8 +5,7 @@ robot = HamBot()
 
 #Inputs: time (float), velocity (int), block (bool)
 def moveForward(t, v, blocking):
-    robot.run_left_motor_for_seconds(t, speed=v, blocking=blocking)
-    robot.run_right_motor_for_seconds(t, speed=v, blocking=blocking)
+    robot.run_motors_for_seconds(t, left_speed=v, right_speed=v)
 
     robot.stop_motors()
 
