c35ecd92e3be4954d43738d0aa53ce4b2edcf3cf
Joaquin <joaquing@usf.edu>
2025-02-14 08:40:36 -0500

    v1.23 - Created a moveForward function - Takes inputs: time, velocity, and blocking

--- NewRobot/joaquinTest.py
+++ NewRobot/joaquinTest.py
@@ -1,8 +1,13 @@
 from robot_systems.robot import HamBot
 
+# Create instance of HamBot
 robot = HamBot()
 
-robot.run_left_motor_for_seconds(3.0, speed=50, blocking=False)
-robot.run_right_motor_for_seconds(3.0, speed=50, blocking=False)
+#Inputs: time (float), velocity (int), block (bool)
+def moveForward(t, v, blocking):
+    robot.run_left_motor_for_seconds(t, speed=v, blocking=blocking)
+    robot.run_right_motor_for_seconds(t, speed=v, blocking=blocking)
 
-robot.stop_motors()
+    robot.stop_motors()
+
+moveForward(5.0, 50, True)
