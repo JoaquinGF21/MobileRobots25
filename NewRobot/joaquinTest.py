0f441395b11b2f8e2c8b2eff40e01dfb489c9d97
Joaquin <joaquing@usf.edu>
2025-02-14 08:04:03 -0500

    v1.23 - IT WORKS

--- NewRobot/joaquinTest.py
+++ NewRobot/joaquinTest.py
@@ -2,7 +2,7 @@ from robot_systems.robot import HamBot
 
 robot = HamBot()
 
-robot.run_left_motor_for_seconds(3.0, speed=25, blocking=False)
-robot.run_right_motor_for_seconds(3.0, speed=25, blocking=False)
+robot.run_left_motor_for_seconds(3.0, speed=50, blocking=False)
+robot.run_right_motor_for_seconds(3.0, speed=50, blocking=False)
 
 robot.stop_motors()
