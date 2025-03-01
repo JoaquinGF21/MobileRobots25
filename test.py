f3eb02738fcf3e656b2eef45c92b0b74206ab39b
Jacob <jacobkyleoct@outlook.com>
2025-03-01 18:44:42 -0500

    v3.0.3.1

--- test.py
+++ test.py
@@ -6,12 +6,18 @@ import time
 Chris_R = HamBot()
 time.sleep(22)
 
-Lidar_l = [89,90,91]
-Lidar_r = [269,270,271]
-Lidar_f = [179,180,181]
-Lidar_b = [359,0,1]
+# Lidar_l = [89,90,91]
+# Lidar_r = [269,270,271]
+# Lidar_f = [179,180,181]
+# Lidar_b = [359,0,1]
 
-while(True):
-    temp_array = Chris_R.get_range_image()
-    front_dist_curr = min(temp_array[Lidar_f[0]],temp_array[Lidar_f[1]],temp_array[Lidar_f[2]])
-    print(front_dist_curr)
+# while(True):
+#     temp_array = Chris_R.get_range_image()
+#     front_dist_curr = min(temp_array[Lidar_f[0]],temp_array[Lidar_f[1]],temp_array[Lidar_f[2]])
+#     print(front_dist_curr)
+temp_array = Chris_R.get_range_image()
+print(temp_array)
+def corner():
+    Chris_R.run_left_motor_for_rotations(0.5,-50)
+    Chris_R.run_right_motor_for_rotations(0.5,50)
+corner()
