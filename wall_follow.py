bfbbb1346420970c67b3abdaffd5ccace282431c
Jacob <jacobkyleoct@outlook.com>
2025-03-04 14:58:29 -0500

    v3.2 completed wall follow

--- wall_follow.py
+++ wall_follow.py
@@ -12,8 +12,16 @@ Lidar_r = [269,270,271]
 Lidar_f = [179,180,181]
 Lidar_b = [359,0,1]
 base_speed = 50
-p = 0.5
+target = 300
+
+kp = 0.1
+ki = 0.01
+kd = 0.05
+
+integral = 0
+prev_error = 0.0
 set_dist = 300
+
 def corner(dir):
     if dir == 'right':
         Chris_R.run_left_motor_for_rotations(0.61,-50,False)
@@ -21,7 +29,26 @@ def corner(dir):
     elif dir == 'left':
         Chris_R.run_left_motor_for_rotations(0.61,50,False)
         Chris_R.run_right_motor_for_rotations(0.61,-50,True)
-            
+
+def PID(target, current,prev_error,integral,dt):
+    err = target - current
+    
+    #Proportional
+    p_term = kp * err
+    
+    #integral
+    integral += err * dt
+    i_term = ki * integral
+    #derivative
+    derivative = (err -prev_error)/dt
+    d_term = kd * derivative
+    
+    adj = p_term + i_term + d_term
+    
+    return adj, err, integral
+    
+    
+    
 
 def wall_follow(dir):
     #initialize current and prev distances along with a temp array to hold the lidar readings
@@ -33,22 +60,35 @@ def wall_follow(dir):
     temp_array = []
     
     while(True):
+        currentTime = time.perf_counter()
+        
         Chris_R.set_left_motor_speed(base_speed + adj)
         Chris_R.set_right_motor_speed(base_speed)
+        
         temp_array = Chris_R.get_range_image()
         front_dist_curr = min(temp_array[Lidar_f[0]],temp_array[Lidar_f[1]],temp_array[Lidar_f[2]])
+        
+        
         if (dir == 'left'):
             side_dist_curr = min(temp_array[Lidar_l[0]],temp_array[Lidar_l[1]],temp_array[Lidar_l[2]])
             print(side_dist_curr)
         if (dir == 'right'):
             side_dist_curr = min(temp_array[Lidar_r[0]],temp_array[Lidar_r[1]],temp_array[Lidar_r[2]])
-        if front_dist_curr <= 300:
+        if front_dist_curr <= target:
             Chris_R.stop_motors()
             corner(dir)
-        err = set_dist - side_dist_curr
-        adj = err * p
-        
             
+        try:
+            dt = currentTime - prevTime
+            adj, prev_error, integral = PID(target,side_dist_curr,prev_error,integral,dt)
+        except:
+            pass
+            
+            
+            
+        prevTime = currentTime
+        
+     
 wall_follow("left")
             
         
