b09a93a3e675800c359b21fc615c99a5daf380df
Jacob <jacobkyleoct@outlook.com>
2025-03-04 17:16:12 -0500

    t9

--- wall_follow.py
+++ wall_follow.py
@@ -24,7 +24,7 @@ set_dist = 300
 def corner(dir):
     temp_array = Chris_R.get_range_image()
     front_dist_curr = min(temp_array[Lidar_f[0]],temp_array[Lidar_f[1]],temp_array[Lidar_f[2]])
-    
+    print(dir)
     if (dir == 'left'):
         while ((temp_array[105]-5 <= temp_array[75] <= temp_array[105]+5) and (front_dist_curr > 350)):
             temp_array = Chris_R.get_range_image()
@@ -37,7 +37,7 @@ def corner(dir):
             front_dist_curr = min(temp_array[Lidar_f[0]],temp_array[Lidar_f[1]],temp_array[Lidar_f[2]])
             Chris_R.set_left_motor_speed(30)
             Chris_R.set_right_motor_speed(-30)
-    print(f"{temp_array[75]} : {temp_array[105]}")
+    
     Chris_R.stop_motors()
 
         
