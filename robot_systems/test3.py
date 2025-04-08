ef4b421c07775533d268e6e4fbe9f87070285d67
Joaquin <joaquing@usf.edu>
2025-04-08 17:36:33 -0400

    temporary test3

--- robot_systems/test3.py
+++ robot_systems/test3.py
@@ -8,8 +8,7 @@ Chris_R = HamBot()
 camera = Chris_R.camera
 color = (158,0,255)
 while(True):
-    image = camera.get_image()
     camera.set_landmark_colors(color,0.1)
-    landmarks = camera.find_landmarks(image)
+    landmarks = camera.find_landmarks()
     print(landmarks)
 
