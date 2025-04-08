c10ead0cf34baa63eb30057bb3746cf5d4f0c859
Jacob <jacobkyleoct@outlook.com>
2025-04-08 17:19:48 -0400

    v4

---
+++ robot_systems/test3.py
@@ -0,0 +1,15 @@
+from robot_systems.robot import HamBot
+from robot_systems.camera import Camera
+import math
+import time
+
+
+Chris_R = HamBot()
+camera = Chris_R.camera
+color = (158,0,255)
+while(True):
+    image = camera.get_image()
+    camera.set_landmark_colors(color,0.1)
+    landmarks = camera.find_landmarks(image)
+    print(landmarks)
+
