c6a6e853baca15a183717f26d0f2758367f332f6
Joaquin <joaquing@usf.edu>
2025-04-12 17:26:55 -0400

    v3.whatTheSigma5

--- testJac.py
+++ testJac.py
@@ -62,6 +62,7 @@ time.sleep(0.5)
 
 
 front_dist_curr = 900
+i = 0
 while(True):
     camera.set_landmark_colors(color,0.1)
     landmarks = camera.find_landmarks()
@@ -70,8 +71,10 @@ while(True):
     Lidar_f = [179,180,181]
     temp_array = Chris_R.get_range_image()
     front_dist_curr = min(temp_array[Lidar_f[0]], temp_array[Lidar_f[1]], temp_array[Lidar_f[2]])
-
-    Chris_R.set_left_motor_speed(max(-75,min(75,base_speed + adj)))
+    if i > 2:
+        Chris_R.set_left_motor_speed(max(-75,min(75,base_speed + adj)))
+    else:
+        Chris_R.set_left_motor_speed(base_speed)
     Chris_R.set_right_motor_speed(base_speed)
     
     dt = currentTime - previousTime
