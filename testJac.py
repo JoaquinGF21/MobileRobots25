f05922e7b6fc0271d0b5ea38f0f93a9139cf8a5f
Joaquin <joaquing@usf.edu>
2025-04-12 15:54:23 -0400

    JacTest1

--- testJac.py
+++ testJac.py
@@ -1,9 +1,10 @@
 from robot_systems.robot import HamBot
 from robot_systems.camera import Camera
+from robot_systems.camera import Landmark
 import math
 import time
 range = 500
-#s
+
 Chris_R = HamBot()
 camera = Chris_R.camera
 color = (158,0,255)
@@ -13,15 +14,7 @@ while(True):
     landmarks = camera.find_landmarks()
     
     if landmarks:
-        print(landmarks[0])
-# def rotate(deg):
-#     axel = 115
-#     wheel_diameter = 90
-#     rotations = (axel * deg) / (360 * wheel_diameter)
-#     Chris_R.run_left_motor_for_rotations(rotations, 20, False)
-#     Chris_R.run_right_motor_for_rotations(-rotations,20, False)
-# rotate(90)
-# time.sleep(7)
-# rotate(-90)
+        print(Landmark.x)
+
 
 
