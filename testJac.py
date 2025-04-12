4c198ba01dff7f7098bccec1ed9856fd7870f506
Joaquin <joaquing@usf.edu>
2025-04-12 16:21:40 -0400

    v3.fuckIfIKnow

--- testJac.py
+++ testJac.py
@@ -14,7 +14,7 @@ while(True):
     
     if landmarks:
         print(landmarks[0].x)
-        if landmarks[0].x == 320:
+        if landmarks[0].x < 320:
             print("I found it!")
             Chris_R.set_left_motor_speed(25)
             Chris_R.set_right_motor_speed(25)
