a372b21489ea52987a660e714fdd2ffeed9b0ad7
Jacob <jacobkyleoct@outlook.com>
2025-04-12 14:52:40 -0400

    v3.8.2 testing some shit i guesss

--- test2.py
+++ test2.py
@@ -6,14 +6,22 @@ range = 500
 
 Chris_R = HamBot()
 camera = Chris_R.camera
-color = (158,0,255)
-camera.set_landmark_colors(color,0.1)
-while(True):
-    camera.set_landmark_colors(color,0.1)
-    landmarks = camera.find_landmarks()
+# color = (158,0,255)
+# camera.set_landmark_colors(color,0.1)
+# while(True):
+#     camera.set_landmark_colors(color,0.1)
+#     landmarks = camera.find_landmarks()
     
-    print(landmarks)
-    print(landmarks[0])
-
+#     print(landmarks)
+#     print(landmarks[0])
+def rotate(deg):
+    axel = 115
+    wheel_diameter = 90
+    rotations = (axel * deg) / (360 * wheel_diameter)
+    Chris_R.run_left_motor_for_rotations(rotations, 20, False)
+    Chris_R.run_right_motor_for_rotations(-rotations,20, False)
+rotate(90)
+time.sleep(7)
+rotate(-90)
 
 
