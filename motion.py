20ebfc13831834a6ff76bd32568b385cdaf95989
Joaquin <joaquing@usf.edu>
2025-04-22 15:57:54 -0400

    v4.02 - Created the rotation and forward functions.

--- motion.py
+++ motion.py
@@ -7,10 +7,14 @@ Chris_R = HamBot()
 time.sleep(2)
 
 def rotate(deg):
-    axel = 160
+    axel = 192
     wheel_diameter = 90
     rotations = (axel * deg) / (360 * wheel_diameter)
-    Chris_R.run_left_motor_for_rotations(rotations, 20, False)
-    Chris_R.run_right_motor_for_rotations(-rotations,20, False)
+    Chris_R.run_left_motor_for_rotations(rotations, 25, False)
+    Chris_R.run_right_motor_for_rotations(-rotations,25, True)
     
-rotate(90)
+def forward(rotations):
+    Chris_R.run_left_motor_for_rotations(rotations, 35, False)
+    Chris_R.run_right_motor_for_rotations(rotations, 35, True)
+    
+forward(3.5)
